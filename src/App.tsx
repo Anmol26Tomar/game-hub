d65b6e99932230d9e4bea22292c3971506248caa
Anmol26Tomar <26anmoltomar@gmail.com>
2024-10-09 19:36:32 +0530

    Refactored code to avoid multiple useState

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,14 @@ import PlatformSelector from "./components/PlatformSelector";
 import { Genre } from "./hooks/useGenres";
 import { useState } from "react";
 import { Platform } from "./hooks/useGames";
+
+export interface GameQuery{
+  genre: Genre | null;
+  platform: Platform | null;
+}
+
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform|null>(null);
+  const [gameQuery, setGameQuery]=useState<GameQuery>({} as GameQuery);
   return (
     <Grid
       templateAreas={{
@@ -29,17 +34,18 @@ function App() {
         {/* Shows for >=large */}
         <GridItem area="aside" px={5}>
           <GenreList
-            selectedGenre={selectedGenre}
-            onSelectGenre={(genre) => setSelectedGenre(genre)}
-          />
+            selectedGenre={gameQuery.genre}
+            onSelectGenre={(genre) => setGameQuery({...gameQuery,genre})}
+            />
         </GridItem>
       </Show>
       <GridItem area="main">
         <PlatformSelector
-          selectedPlatform={selectedPlatform}
-          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          selectedPlatform={gameQuery.platform}
+          onSelectPlatform={(platform) => setGameQuery({...gameQuery,platform})}
         />
-        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre} />
+        <GameGrid 
+         gameQuery={gameQuery} />
       </GridItem> 
     </Grid>
   );
