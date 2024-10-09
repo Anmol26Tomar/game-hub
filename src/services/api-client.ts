c8e1c168f4bc5bf2b8727536c3dbee45ffc25b34
Anmol26Tomar <26anmoltomar@gmail.com>
2024-10-09 18:52:46 +0530

    Platform selector menu

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 export default axios.create({
         baseURL:'https://api.rawg.io/api',
         params:{
-            key:'10a17bc1ed614654a970424bc43dce5a'
+            key:'8b6c8dd9e76d4d39ad8eb1035f615fd5'
         }
     }
 )
