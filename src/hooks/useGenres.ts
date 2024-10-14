c55c187feaef8c2bdc15ec115d9807defa45737f
Anmol26Tomar <26anmoltomar@gmail.com>
2024-10-14 19:46:28 +0530

    Ship genres without API call

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,8 @@
-import useData from "./useData"
+import genres from "../data/genres";
 export interface Genre {
     name:string;
     id:number;
     image_background: string;
   }
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => ({data:genres, loading:false, error:null})
 export default useGenres;
