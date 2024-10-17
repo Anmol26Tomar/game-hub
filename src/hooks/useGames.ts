1e7775389153880fadc8ff0cc602c40bf1ebfa1a
Anmol26Tomar <26anmoltomar@gmail.com>
2024-10-17 18:58:22 +0530

    Fetched platforms using React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,13 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import useData, { FetchResponse } from "./useData";
 import { Genre } from "./useGenres";
-
+import apiClient from "../services/api-client";
 export interface Platform {
   id: number;
   name: string;
   slug: string;
 }
-
 export interface Game {
   id: number;
   name: string;
@@ -16,19 +16,25 @@ export interface Game {
   metacritic: number;
   rating_top: number;
 }
-
 const useGames = (gameQuery: GameQuery) =>
+  useQuery({
+    queryKey:["games", gameQuery],
+    queryFn: ()=> 
+      apiClient.get<FetchResponse<Game>>("/games",{
+        params: {
+          genres: gameQuery.genre?.id,
+          platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText
+        },
+      })
+      .then(   )
+  })
   useData<Game>(
     "/games",
     {
-      params: {
-        genres: gameQuery.genre?.id,
-        platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
-      },
+     
     },
     [gameQuery]
   );
-
 export default useGames;
