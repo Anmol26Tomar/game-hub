953a0173e72a98c2b37029c80f25c85a0aa72558
Anmol26Tomar <26anmoltomar@gmail.com>
2024-10-22 16:28:58 +0530

    Built game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,6 +1,5 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
-
-
 export interface Game {
   id: number;
   name: string;
@@ -10,4 +9,6 @@ export interface Game {
   rating_top: number;
   description_raw: string;
   parent_platforms: { platform: Platform; }[];
+  genres:Genre[];
+  publishers:Platform[];
 }
